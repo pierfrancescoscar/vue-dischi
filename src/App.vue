bcd02ea36fec02a05f23fbe4f533966c5f6e50ad
pierfrancescoscar <scarinci401@gmail.com>
2021-11-25 16:15:19 +0100

    FIX: clean useless imports

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,12 @@
 <script>
 import Header from '@/components/Header.vue';
 import Main from '@/components/Main.vue';
-import Card from '@/components/Card.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
     Main,
-    Card,
     
   }
 }
