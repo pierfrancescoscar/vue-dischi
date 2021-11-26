3d375dcf828c4c6b50f6dabaaf5ba087153e7bf7
pierfrancescoscar <scarinci401@gmail.com>
2021-11-26 14:47:22 +0100

    ADD: customized select tag on header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,20 @@
 <template>
   <header>
-      <section class="container-fluid p-3">
-          <img src="../assets/logo (2).png" alt="logo">
+      <section class="container-fluid p-3 d-flex align-items-center">
+          <div class="col-6 logo-box">
+            <img src="../assets/logo (2).png" alt="logo">
+          </div>
+          <div class="col-6 select-box d-flex justify-content-end">
+              <select>
+                  <option value="" disabled selected hidden>Choose your genre</option>
+                  <option value="">All genres</option>
+                  <option value="">Rock</option>
+                  <option value="">Pop</option>
+                  <option value="">Jazz</option>
+                  <option value="">Metal</option>
+                  <option value="">Rock</option>
+              </select>
+          </div>
       </section>
   </header>
 </template>
@@ -13,11 +26,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '@/styles/vars.scss';
 
 header {
     background-color: #2e3a46;
 }
 img {
     width: 50px;
+}
+
+.select-box {
+    select {
+        background-color: $bg-color;
+        color: white;
+        border: 1px solid #1DB954;
+        padding: 10px 35px;
+    }
 }
 </style>
